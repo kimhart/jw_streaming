3e10adcb18f308a9c28d9fbe9bbc1eb12ccf6b49
Kim Hart <kimhart90@gmail.com>
2017-03-23 13:07:33 -0400

    film categories mapped, mostly styled

--- public/main.js
+++ public/main.js
@@ -1,7 +1,50 @@
 
+const carousel = document.querySelector('.carousel-content');
+const documentariesCategory = document.getElementById('documentaries');
+const liveEventsCategory = document.getElementById('live-events');
+const shortFilmsCategory = document.getElementById('short-films');
+
 fetch('https://content.jwplatform.com/feeds/f49AJ8N4.json').then(function(response) { 
-  return response.json();
-}).then(function(j) {
-  console.log(j); 
+    return response.json();
+  }).then(function(j) {
+    populateCategories(j.playlist);
 });
+
+function populateCategories(films) {
+  const documentaries = [];
+  const liveEvents = [];
+  const shortFilms = [];
+  for (i = 0; i <= films.length; i++) {
+    if (i <= 8) {
+      documentaries.push(films[i]);
+    } else if (i > 8 && i <= 13) {
+      liveEvents.push(films[i]);
+    } else {
+      shortFilms.push(films[i]);
+    }
+  }
+  listFilms(documentaries, documentariesCategory);
+  listFilms(liveEvents, liveEventsCategory);
+  listFilms(shortFilms, shortFilmsCategory);
+}
+
+
+function listFilms(list, category) {
+  for (i = 0; i < list.length; i++) {
+
+    const film = document.createElement('div');
+    const titleBar = document.createElement('div');
+    const title = document.createTextNode(list[i].title);
+
+    film.classList.add('film');
+    titleBar.classList.add('title-bar');
+    film.style.backgroundImage = "url('http:" + list[i].image + "')";
+
+    titleBar.appendChild(title);
+    film.appendChild(titleBar);
+    category.appendChild(film);
+  }
+};
+
+
 
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-var express = require('express');
-var app = express();
-var port = process.env.PORT || 3000;
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
 
 app.use('/', express.static('public'));
 
